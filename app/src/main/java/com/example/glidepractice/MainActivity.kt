faea67f1c0e2a668dc391999c697f11e93d940a7
渡辺恭平 <kingdomhearts510@gmail.com>
2021-03-29 19:13:23 +0900

    imageOne表示官僚

--- app/src/main/java/com/example/glidepractice/MainActivity.kt
+++ app/src/main/java/com/example/glidepractice/MainActivity.kt
@@ -2,10 +2,23 @@ package com.example.glidepractice
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
+import com.bumptech.glide.Glide
 
 class MainActivity : AppCompatActivity() {
+
+    private val image = "https://cdn.pixabay.com/photo/2018/05/03/21/49/android-3372580_960_720.png"
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val imageOne = findViewById<ImageView>(R.id.image_one)
+        val imageTwo = findViewById<ImageView>(R.id.image_two)
+        val imageThree = findViewById<ImageView>(R.id.image_three)
+
+        Glide.with(this)
+                .load(image)
+                .into(imageOne)
     }
 }
